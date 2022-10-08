3f5bf73a48d28626441b80d28c3cc2fd0d015859
Smiley <jcsmileyjr@gmail.com>
2022-10-08 11:32:39 -0500

    Feature: Admin Page initial card layout and styling

--- interview-helper/src/pages/adminPage/AdminPage.js
+++ interview-helper/src/pages/adminPage/AdminPage.js
@@ -1,10 +1,11 @@
 import './adminPage.css';
+import PlusSign from "../../images/plus-sign.png";
 
 /**
  * Setup HTML & Content architecture - DONE
- * Setup section design responsiveness
- *  - Mobile
- *  - Desktop
+ * Setup section design responsiveness - DONE
+ *  - Mobile - DONE
+ *  - Desktop - DONE
  * Setup styling for page elements
  *  - Get one card looking correct
  *  - Break off into a component
@@ -20,103 +21,20 @@ const AdmingPage = () => {
             <main className="adminPage__content--container">
                 <p className="adminPage__pageTitle--style">Choose One</p>
                 <div className="adminPage__card--container">
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
+                    <article className="card">                        
+                        <div className="card__title--container card__content--margin">
+                            <p>Topic: Teamwork</p>
+                        </div>
+                        <div className="card__content--margin card__question--container">
+                            <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
+                        </div>
+                        <div className="card__content--margin card__details--container">
+                            <img className="card__image--style" src={PlusSign} alt="clickable plus sign to provide more details on question" /> More
+                        </div>
+                        <div className="card__button--container">
+                            <button type="button" className="card__button--style">Practice</button> 
                         </div>
                     </article>
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
-                        </div>
-                    </article>
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
-                        </div>
-                    </article>  
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
-                        </div>
-                    </article>
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
-                        </div>
-                    </article>
-                    <article>
-                        <div class="card">
-                            <div>
-                                <p>Topic: Teamwork</p>
-                            </div>
-                            <div>
-                                <p>Tell me about a time when you had to work closely with someone whose personality was very different from yours?</p>
-                            </div>
-                            <div>
-                                <span>plus</span> More
-                            </div>
-                            <div>
-                                <button type="button">Practice</button> 
-                            </div>
-                        </div>
-                    </article>                                                   
-
                 </div>
             </main>
         </div>
