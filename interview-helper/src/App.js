e431da4a955df6b26011a29dde4ce28964da97ee
Smiley <jcsmileyjr@gmail.com>
2022-10-09 17:41:01 -0500

    Feature: Recording page file setup and initial layout

--- interview-helper/src/App.js
+++ interview-helper/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LandingPage from './pages/landingPage/LandingPage';
 import AdmingPage from './pages/adminPage/AdminPage';
+import RecordingPage from './pages/recordingPage/RecordingPage';
 
 function App() {
   return (
     <div className="App">
-      <AdmingPage />
+      {/* <AdmingPage /> */}
       {/* <LandingPage /> */}
+      <RecordingPage />
     </div>
   );
 }
---
+++ interview-helper/src/pages/recordingPage/RecordingPage.js
@@ -0,0 +1,50 @@
+import "./recordingPage.css";
+import Noise from "../../images/noise-icon.png";
+
+const RecordingPage = () => {
+  return (
+    <div className="recordingPage--container">
+      <header>
+        <p className="header__title--style">Behavior Interview Helper</p>
+      </header>
+      <main>
+        <section className="recordingPage__image--container">
+          <h1>Playing</h1>
+          <img src={Noise} alt="icon of sound" />
+        </section>
+        <div className="recordingPage__content--container">
+          <section className="content__recording--container">
+            <h3>Recording</h3>
+            <p>
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+          </section>
+          <section className="content__previousAnswer--container">
+            <h3>Previous Answer</h3>
+            <p>
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+          </section>
+        </div>
+        <div className="recordingPage__controls--container">
+          <button type="button">Play</button>
+          <button type="button">Pause</button>
+          <button type="button">Stop</button>
+          <button type="button">New Question</button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default RecordingPage;
