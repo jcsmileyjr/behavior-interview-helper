6fb3adf86fe4efdc4d912f8c5cd16f956d851211
Smiley <jcsmileyjr@gmail.com>
2022-10-09 11:21:56 -0500

    Feature: Question card component clickable intent functionality

--- interview-helper/src/component/questionCard/QuestionCard.js
+++ interview-helper/src/component/questionCard/QuestionCard.js
@@ -1,8 +1,13 @@
+import React, { useState} from "react";
 import './questionCard.css';
 import PlusSign from "../../images/plus-sign.png";
+import MinusSign from "../../images/minus-sign.png";
 
 const QuestionCard = ({question}) => {
-    const {topic, content} = question;
+    const {topic, content, intent} = question;
+
+    const [showIntent, setIntent] = useState(false);
+
     return(
         <article className="card">                        
             <div className="card__title--container card__content--margin">
@@ -11,8 +16,13 @@ const QuestionCard = ({question}) => {
             <div className="card__content--margin card__question--container">
                 <p>{content}</p>
             </div>
-            <div className="card__content--margin card__details--container">
-                <img className="card__image--style" src={PlusSign} alt="clickable plus sign to provide more details on question" /> More
+            <div onClick={()=> setIntent(!showIntent)} className="card__content--margin card__details--container">
+                {!showIntent &&
+                    <span><img className="card__image--style" src={PlusSign} alt="clickable plus sign to provide more details on question" /> More</span>
+                }
+                {showIntent &&
+                    <p><img className="card__image--style" src={MinusSign} alt="clickable minus sign to hide extra details on question" /> {intent}</p>
+                }
             </div>
             <div className="card__button--container">
                 <button type="button" className="card__button--style">Practice</button> 
